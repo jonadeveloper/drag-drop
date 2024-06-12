97b1bbca464dbfcc39113fb181101bb89a49e903
= <=>
2024-06-12 20:52:26 -0300

    roadmap web developer drag and drop

---
+++ script.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const listItems = document.querySelectorAll("#languages-list li");
+  let dragSrcEl = null;
+
+  function handleDragStart(e) {
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", this.innerHTML);
+    this.classList.add("dragging");
+  }
+
+  function handleDragOver(e) {
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.dataTransfer.dropEffect = "move";
+    return false;
+  }
+
+  function handleDragEnter() {
+    this.classList.add("over");
+  }
+
+  function handleDragLeave() {
+    this.classList.remove("over");
+  }
+
+  function handleDrop(e) {
+    if (e.stopPropagation) {
+      e.stopPropagation();
+    }
+
+    if (dragSrcEl !== this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData("text/html");
+    }
+
+    return false;
+  }
+
+  function handleDragEnd() {
+    listItems.forEach((item) => {
+      item.classList.remove("over");
+      item.classList.remove("dragging");
+    });
+  }
+
+  listItems.forEach((item) => {
+    item.addEventListener("dragstart", handleDragStart);
+    item.addEventListener("dragenter", handleDragEnter);
+    item.addEventListener("dragover", handleDragOver);
+    item.addEventListener("dragleave", handleDragLeave);
+    item.addEventListener("drop", handleDrop);
+    item.addEventListener("dragend", handleDragEnd);
+  });
+});
